8cc4119b2b378d1028fc93a7978175fc77132b40
Evgeny Grigorev <e.grigoriev@aerodisk.ru>
2024-07-23 17:50:59 +0300

    Hello world

---
+++ drivers/my_mods/hello_world/hello.c
@@ -0,0 +1,22 @@
+#include "asm/current.h"
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+
+
+MODULE_LICENSE("Dual BSD/GPL");
+
+
+static int __init hello_init(void) {
+    printk(KERN_INFO "The process is \"%s\" (pid %i)\n", current->comm, current->pid);
+    return 0;
+}
+
+
+static void __exit hello_exit(void) {
+    printk("Goodbye, World!\n");
+}
+
+
+module_init(hello_init);
+module_exit(hello_exit);
